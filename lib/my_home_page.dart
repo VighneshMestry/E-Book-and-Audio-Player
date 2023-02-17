9ae75e83057608ebec1b7192b3258842d3c261a1
Vighnesh Mestry <mestryvighnesh27@gmail.com>
2023-02-18 03:48:02 +0530

    step-1

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -15,8 +15,78 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Container(
       color: AppColors.background,
-      child: const SafeArea(
-        child: Scaffold(),
+      child: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(Icons.menu),
+                    Row(
+                      children: const [
+                        Icon(Icons.search),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Icon(Icons.notifications),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: const Text(
+                      "Popular Books",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                height: 180,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      left: -20,
+                      right: 0,
+                      child: Container(
+                        height: 180,
+                        // PageView runs in loops
+                        child: PageView.builder(
+                            controller: PageController(viewportFraction: 0.8),
+                            itemCount: 5,
+                            itemBuilder: (_, i) {
+                              return Container(
+                                height: 180,
+                                width: MediaQuery.of(context).size.width,
+                                margin: const EdgeInsets.only(right: 10),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: const DecorationImage(
+                                      image: AssetImage(
+                                          "img/think_and_grow_rich.jpg"),
+                                      fit: BoxFit.fill),
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
